868e81e3e718400c6c9b6778d7255c59fe6b17fb
张君 <zhangjun.paul@bytedance.com>
2019-04-05 19:08:28 +0800

    Mod: 修改

--- app/src/main/java/com/zj/fastroute/SecondActivity.java
+++ app/src/main/java/com/zj/fastroute/SecondActivity.java
@@ -4,16 +4,21 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.zj.annotation.Inject;
 import com.zj.annotation.Route;
+import com.zj.api.core.KRouter;
 
 /**
  * Created by zhangjun on 2018/1/24.
  */
 @Route(path = "second")
 public class SecondActivity extends AppCompatActivity {
+    @Inject(name = "int_value")
+    private int valueFromIntent;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        KRouter.INSTANCE.inject(this);
     }
 }
